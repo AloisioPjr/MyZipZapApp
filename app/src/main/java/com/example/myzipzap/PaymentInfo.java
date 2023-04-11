00283bf39fb973750890f1013d93c101b8952096
Juan23Coding <114172189+Juan23Coding@users.noreply.github.com>
2023-04-11 22:29:21 +0100

    Papi Juancho Databinding CheckoutActivity

--- app/src/main/java/com/example/myzipzap/PaymentInfo.java
+++ app/src/main/java/com/example/myzipzap/PaymentInfo.java
@@ -2,14 +2,105 @@ package com.example.myzipzap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.google.android.gms.common.internal.Constants;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wallet.IsReadyToPayRequest;
+import com.google.android.gms.wallet.PaymentsClient;
+import com.google.android.gms.wallet.Wallet;
+import com.google.android.gms.wallet.WalletConstants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Optional;
+
 public class PaymentInfo extends AppCompatActivity {
+    public static final BigDecimal CENTS_IN_A_UNIT = new BigDecimal(100);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_info);
+    }
+
+    private static JSONObject getBaseRequest() throws JSONException {
+        return new JSONObject().put("apiVersion", 2).put("apiVersionMinor", 0);
+    }
+
+    private static JSONObject getGatewayTokenizationSpecification() throws JSONException {
+        return new JSONObject() {{
+            put("type", "PAYMENT_GATEWAY");
+            put("parameter", new JSONObject() {{
+                put("gateway", "example");
+                put("gatewayMerchantId", "exampleGatewayMerchantId");
+            }});
+        }};
+    }
+
+    private static JSONArray getAllowedCardNetworks() {
+        return new JSONArray()
+                .put("MASTERCARD")
+                .put("VISA");
+    }
+
+    private static JSONArray getAllowedCardAuthMethods() {
+        return new JSONArray()
+                .put("PAN_ONLY")
+                .put("CRYPTOGRAM_3DS");
+    }
+
+    private static JSONObject getBaseCardPaymentMethod() throws JSONException {
+        JSONObject cardPaymentMethod = new JSONObject();
+        cardPaymentMethod.put("type", "card");
+
+        JSONObject parameters = new JSONObject();
+        parameters.put("allowedAuthMethods", getAllowedCardAuthMethods());
+        parameters.put("allowedCardNetworks", getAllowedCardNetworks());
+        parameters.put("billingAddressRequired", true);
+
+        JSONObject billingAddressParameters = new JSONObject();
+        billingAddressParameters.put("format", "FULL");
+        parameters.put("billingAddressParameters", billingAddressParameters);
+        cardPaymentMethod.put("parameters", parameters);
+
+        return cardPaymentMethod;
+    }
+
+    private static JSONObject getCardPaymentMethod() throws JSONException {
+        JSONObject cardPaymentMethod = getBaseCardPaymentMethod();
+        cardPaymentMethod.put("tokenizationSpecification", getGatewayTokenizationSpecification());
+
+        return cardPaymentMethod;
+    }
+
+    public static PaymentsClient createPaymentsClient(Context context) {
+        Wallet.WalletOptions walletOptions =
+                new Wallet.WalletOptions.Builder().setEnvironment(WalletConstants.ENVIRONMENT_TEST).build();
+        return Wallet.getPaymentsClient(context, walletOptions);
+    }
+
+    public static Optional<JSONObject> getIsReadyToPayRequest() {
+        try {
+            JSONObject isReadyToPayRequest = getBaseRequest();
+            isReadyToPayRequest.put(
+                    "allowedPaymentMethods", new JSONArray().put(getBaseCardPaymentMethod()));
+            return Optional.of(isReadyToPayRequest);
+        }catch(JSONException e) {
+            return Optional.empty();
+        }
+    }
+
+    public static String centsToString(long cents) {
+        return new BigDecimal(cents)
+                .divide(CENTS_IN_A_UNIT, RoundingMode.HALF_EVEN)
+                .setScale(2, RoundingMode.HALF_EVEN)
+                .toString();
     }
 
 
