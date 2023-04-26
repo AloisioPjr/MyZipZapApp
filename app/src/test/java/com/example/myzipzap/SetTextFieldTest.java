a2fa8839335ec38c87fb8377b1f4ed24f30af329
AloisioPjr <Aloisiopjr@outlook.com>
2023-04-26 20:04:55 +0100

    Finished 4

--- app/src/test/java/com/example/myzipzap/SetTextFieldTest.java
+++ app/src/test/java/com/example/myzipzap/SetTextFieldTest.java
@@ -14,6 +14,15 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import android.widget.TextView;
 import static org.mockito.Mockito.*;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 @RunWith(MockitoJUnitRunner.class)
 public class SetTextFieldTest {
     @Mock
@@ -48,5 +57,53 @@ public class SetTextFieldTest {
         verify(balText).setText("Balance: €" + expectedBalance / 100);
         verify(idText).setText(expectedUserId);
         verify(emailText).setText(expectedEmail);
+    }
+    @Test
+    public void testSetTextFields2() {
+        // create an instance of the class that contains this method
+        QRScanner myClass = new QRScanner();
+        long expectedBalance = 100L;
+        String expectedUserId = "testUser";
+        String expectedEmail = "testEmail@example.com";
+        // set up any required data or objects
+        myClass.dbCredit = expectedBalance;
+        myClass.userId = expectedUserId;
+        myClass.email = expectedEmail;
+        myClass.balText = balText;
+        myClass.idText = idText;
+        myClass.emailText = emailText;
+        // Call the method under test
+        myClass.setTextFields();
+        // call the method to be tested
+        myClass.setTextFields();
+
+        // assert that each statement has been executed
+        assertEquals("Balance: €" + expectedBalance / 100, balText.getText());
+        assertEquals(expectedUserId, idText.getText());
+        assertEquals(expectedEmail, emailText.getText());
+    }
+    @Test
+    public void testFirebaseDatabaseWrite() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        // Write a value to the database
+        myRef.setValue("Hello, World!");
+        myRef = database.getReference("message");
+        // Read the value from the database and assert that it matches the expected value
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                assertEquals("Hello, World!", value);
+            }
+
+
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                fail("Test was cancelled: " + error.getMessage());
+            }
+        });
     }
 }
